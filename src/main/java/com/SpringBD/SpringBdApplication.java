b3ecbbcecf8b119be6445b481a8e6e1405e64108
luis perylo <lfpb@cesar.school>
2025-04-21 20:43:37 -0300

    estrutura organizada

---
+++ src/main/java/com/SpringBD/SpringBdApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBdApplication.class, args);
+	}
+
+}
